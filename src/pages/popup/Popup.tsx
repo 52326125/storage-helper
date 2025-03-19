f9f67e3ce526c12487e8d6bdd9a5fb26755ab4d3
莊銪盛 <sam@Mac-mini.local>
2025-03-19 16:49:29 +0800

    Add ui,chrome storage usage

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,24 +1,70 @@
-import React from 'react';
-import logo from '@assets/img/logo.svg';
+import { useCallback, useEffect, useState } from "react";
+import Button from "./component/Button";
+import ConfigurationItem from "./component/ConfigurationItem";
+import { v4 } from "uuid";
+import { Configuration } from "@src/interface/storeage";
 
 export default function Popup() {
+  const EMPTY_CONFIGURATION: Configuration = {
+    id: "",
+    domain: "",
+    storage: [
+      {
+        type: undefined,
+        key: "",
+        value: ""
+      }
+    ]
+  };
+  const [configurations, setConfigurations] = useState<Configuration[]>([]);
+
+  const handleAddClick = () => {
+    const configuration = {
+      ...EMPTY_CONFIGURATION,
+      id: v4()
+    };
+    setConfigurations([...configurations, configuration]);
+  };
+
+  const handleSaveClick = () => {
+    chrome.storage.local.set({ configurations });
+  };
+
+  useEffect(() => {
+    const getConfiguration = async () => {
+      const savedConfiguration = await chrome.storage.local.get("configurations");
+
+      if (!savedConfiguration.configurations) return;
+      setConfigurations(savedConfiguration.configurations as Configuration[]);
+    };
+
+    getConfiguration();
+  }, []);
+
   return (
-    <div className="absolute top-0 left-0 right-0 bottom-0 text-center h-full p-3 bg-gray-800">
-      <header className="flex flex-col items-center justify-center text-white">
-        <img src={logo} className="h-36 pointer-events-none animate-spin-slow" alt="logo" />
-        <p>
-          Edit <code>src/pages/popup/Popup.jsx</code> and save to reload.
-        </p>
-        <a
-          className="text-blue-400"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-        <p>Popup styled with TailwindCSS!</p>
+    <div className="h-full p-3 bg-gray-800 overflow-auto max-h-full">
+      <header className="flex flex-col items-center justify-center text-white mb-3">
+        <h1 className="text-2xl">Storage Helper</h1>
       </header>
+      <div className="flex flex-col items-start justify-center gap-3">
+        {configurations.map((configuration, index) => (
+          <ConfigurationItem key={index} configuration={configuration} setConfiguration={setConfigurations} />
+        ))}
+        <div className="flex gap-2">
+          <Button onClick={handleAddClick}>
+            <div className="flex items-center gap-1.5">
+              <img src="/add.png" alt="Delete" className="w-[20px]" />
+              Add Configuration
+            </div>
+          </Button>
+          <Button onClick={handleSaveClick}>
+            <div className="flex items-center gap-1.5">
+              <img src="/save.png" alt="Delete" className="w-[20px]" />
+              save
+            </div>
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
